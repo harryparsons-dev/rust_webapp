79b7be8408aae4c987c33f2aa7a4f544a48ca584
Harry Parsons <harry.parsons11@outlook.com>
2023-02-11 17:25:27 +0000

    4

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,6 @@
-#[macro_use] extern crate nickel;
-extern crate chrono;
+extern crate clap;
+use clap::App;
 
-use nickel::Nickel;
-use chrono::{DateTime, Local};
-use std::io::prelude::*;
-use std::fs::{File, OpenOptions};
-use std::io;
-
-fn do_log_time() -> String{
-
-
-     match log_time("log.txt"){
-        Ok(entry) => format!("Entry Logged: {}", entry),
-        Err(e) => format!("Error: {}", e)
-     }
-
-
-}
-
-fn log_time(filename: &'static str) -> io::Result<String> {
-    let entry = formatted_time_entry();
-    {
-        let bytes = entry.as_bytes();
-
-        record_entry_log(filename, &bytes)?;
-    }
-    Ok(entry)
-
-}
-
-fn formatted_time_entry() -> String{
-    let local: DateTime<Local> = Local::now();
-    let formatted = local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string();
-    formatted
-}
-
-fn record_entry_log(filename: &str, bytes: &[u8]) -> io::Result<()>{
-    let mut file = (OpenOptions::new().
-                    append(true).
-                    write(true).
-                    create(true).
-                    open(filename))?;
-    (file.write_all(bytes))?;
-    Ok(())
-}
-
-
-
-
-fn main() {
-    let mut server = Nickel::new();
-
-    server.utilize(router! {
-        get "**" => |_req, _res| {
-            do_log_time()
-        }
-    });
-
-    server.listen("127.0.0.1:6767").unwrap();
-}
-
-fn say_hello() -> &'static str {
-    "Hello world! (From function)"
+fn main(){
+    let _ = App::new("fake").version("v1.0-beta").get_matches();
 }
